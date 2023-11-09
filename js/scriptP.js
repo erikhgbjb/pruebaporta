0255a620e705328b9e854b30d47ca6197afe22cb
Erik Quintero <kireodraude2@hotmail.com>
2023-11-09 14:31:48 -0600

    mas vistas agregadas

--- js/scriptP.js
+++ js/scriptP.js
@@ -12,7 +12,16 @@ function stilys(){
         document.write('<link rel="stylesheet" href="style/styleMEX.css" id="inc">');
     }else if ( (month == 12 && day > 16) || (month == 1 && day < 12) ){
         document.write('<link rel="stylesheet" href="style/styleXmas.css" id="inc">');
-    }else if( (month == 2 && day >3) && (month == 2 && day < 7) ){
+    }else if( (month == 2 && day > 3) && (month == 2 && day < 7) ){
+        document.write('<link rel="stylesheet" href="style/styleMEX.css" id="inc">');
+    }else if( (month == 2 && day > 12) && (month == 2 && day < 16) ){
+        document.write('<link rel="stylesheet" href="style/styleDAA.css" id="inc">');
+    }else if( (month == 2 && day > 22) && (month == 2 && day < 26) ){
+        document.write('<link rel="stylesheet" href="style/styleMEX.css" id="inc">');
+    }else if( (month == 3 && day > 19) && (month == 3 && day < 22) ){
+        document.write('<link rel="stylesheet" href="style/styleEP.css" id="inc">');
+    }//podrian faltar el 30 de abril, 1 de mayo y 10 de mayo
+    else if( (month == 9 && day >0) && (month == 9 && day < 31) ){
         document.write('<link rel="stylesheet" href="style/styleMEX.css" id="inc">');
     }
 }
