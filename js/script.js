ca02ac40abd13d14e99ec8ed79424dc2e9aecae1
erikhgbjb <149908985+erikhgbjb@users.noreply.github.com>
2023-11-15 21:49:37 -0600

    día niño 1

--- js/script.js
+++ js/script.js
@@ -56,8 +56,8 @@ function backg(){
     "http://drive.google.com/uc?export=view&id=1Dg8OEZWMM4ttK2KSzdw0MqqmiNw1g0TP","http://drive.google.com/uc?export=view&id=1hmQQuZO7wvPnxRcnehO3QHHe2WQmC4kZ",
     "http://drive.google.com/uc?export=view&id=1bfOIAH9oqhcHRl-tftLIxlENJArVS-us"];
 
-    month=5;
-    day=1;
+    month=4;
+    day=30;
 
     if ((month == 10 && day > 20) && (month == 11 && day < 8) ) {//muertos
         fondo(DM[aleatorio(DM.length)]);
